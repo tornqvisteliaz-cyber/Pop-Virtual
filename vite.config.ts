e4ce5c1db885f9e4d8131053d9c5cd5956b401ac
Eliaz Törnqvist <tornqvisteliaz@gmail.com>
2026-02-15 15:11:17 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+server: {
+host: "::",
+port: 8080,
+hmr: {
+overlay: false,
+},
+},
+base: "/Pop-Virtual/",
+plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+resolve: {
+alias: {
+"@": path.resolve(__dirname, "./src"),
+},
+},
 }));
